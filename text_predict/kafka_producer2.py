b92109cf5d88ec55e5acbfff45da326d09fc51ce
Anmisha Reddy <anmisha.reddy@gmail.com>
2018-02-03 10:33:55 -0500

    Update kafka_producer2.py

--- text_predict/kafka_producer2.py
+++ text_predict/kafka_producer2.py
@@ -14,44 +14,15 @@ class Producer(object):
 
     def produce_msgs(self, source_symbol):
         topic ='twitter_stream'
-        """
         
-        opener = urllib.URLopener()
-        myurl = "https://s3-us-west-2.amazonaws.com/timo-twitter-data/2016-02-08-11-57_tweets.txt"
-        myfile = opener.open(myurl)
-
-        for line in myfile:
-            try:
-                d = json.loads(line)
-                #print d
-            except: 
-                continue
-
-            
-
-            p=""
-            p=p+("id:")
-            p=p+str(d['id'])
-            p=p+(",lang:")
-            p=p+str(d['lang'])
-            p=p+(",text:")
-            p=p+str(d['text'])
-            p=p+(",count:")
-            p=p+str(d['user']['followers_count'])
-
-            print json.dumps(p)
-
-            self.producer.send(topic, p)
-            
-            """
 
 
 #Variables that contains the user credentials to access Twitter API
     
-        consumer_key="dIhjRcszok2R3LyK0VgYSded8"
-        consumer_secret="IxujimHa6JJcWS8mzfC57i9GbgQSc7qst49Fvqu0aglyuykecQ"
-        access_token="2493349752-ao5qcIqhAga9jdk2N6RJTxeRIuQXaFwRk9RUhRb"
-        access_token_secret="T0sYiVKJQsYRShQopO1Kvqwbr7Bx0Ky3DXs8kdRuxEUTZ"
+        consumer_key="consumer_key"
+        consumer_secret="consumer_secret"
+        access_token="access_token"
+        access_token_secret="a_token_secret"
         count=0
         
 
@@ -60,10 +31,7 @@ class Producer(object):
 
         stream = TwitterStream(domain='userstream.twitter.com', auth = auth, secure = True)
 
-
-        search_term = "Aorus, MSI, Asus, Hp, Lenovo, Samsung, Apple, Razor,Dell"
-
-        #tweet_iter = stream.statuses.filter(track = search_term, language = 'en')
+       
         tweet_iter = stream.statuses.sample()
         for iter in tweet_iter:
             count = count +1 
